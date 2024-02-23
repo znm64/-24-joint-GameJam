4b77897d67808db641cd79ab312d85660a943554
Zubin M <102535933+znm64@users.noreply.github.com>
2024-02-23 22:50:54 +0000

    post-processing changes - not finished

---
+++ Platformer 2/Assets/Scripts/PostProcessingControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+PostProcessVolume QuickVolume(int layer, float priority, params PostProcessEffectSettings[] settings);
+
+public class VignettePulse : MonoBehaviour
+{
+   PostProcessVolume m_Volume;
+   Vignette m_Vignette;
+   void Start()
+  {
+      // Create an instance of a vignette
+       m_Vignette = ScriptableObject.CreateInstance<Vignette>();
+       m_Vignette.enabled.Override(true);
+       m_Vignette.intensity.Override(1f);
+      // Use the QuickVolume method to create a volume with a priority of 100, and assign the vignette to this volume
+       m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
+  }
+
+   void Update()
+  {
+       // Change vignette intensity using a sinus curve
+        m_Vignette.intensity.value = (Mathf.Sin(Time.realtimeSinceStartup)+1)/2;
+        Debug.Log((Mathf.Sin(Time.realtimeSinceStartup)+1)/2);
+  }
+}
