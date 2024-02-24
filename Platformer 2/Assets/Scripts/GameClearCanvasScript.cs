c8294667b4ad18da47221e3ebe2bd88c2968d494
Excalibur643 <156510214+Excalibur643@users.noreply.github.com>
2024-02-24 16:59:30 +0000

    Added Game Clear Scene

--- Platformer 2/Assets/Scripts/GameClearCanvasScript.cs
+++ Platformer 2/Assets/Scripts/GameClearCanvasScript.cs
@@ -2,24 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameOverMenuScript : MonoBehaviour
+public class GameClearCanvasScript : MonoBehaviour
 {
-    [SerializeField] public GameObject gameOverCanvas;
+    [SerializeField] public GameObject gameClearCanvas;
     // Start is called before the first frame update
     void Start()
     {
-        //gameOverCanvas.SetActive(true);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    public void reincarnateButton()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
     }
 
     public void returnToMenuButton()
